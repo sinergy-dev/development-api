384dd20519e16120fefc5afbe19e2ecf2729f860
rama11 <prof.agastyo@gmail.com>
2020-06-09 09:32:28 +0700

    add Payment Model

--- app/Payment.php
+++ app/Payment.php
@@ -13,8 +13,13 @@ class Payment extends Model
 	protected $primaryKey = 'id';
 	
 	public $timestamps = false;
+
+	protected $appends = [
+		'payment_invoice_URL'
+	];
 	
 	protected $fillable = [
+		'id_job',
 		'id_history',
 		'payment_to',
 		'payment_from',
@@ -24,7 +29,20 @@ class Payment extends Model
 		'date_add'
 	];
 
+	public function getPaymentInvoiceURLAttribute(){
+		return env('APP_URL') . "/" . $this->payment_invoice;
+	}
+
 	public function progress(){
 		return $this->hasMany('App\Payment_history','id_payment','id');
 	}
+
+	public function lastest_progress(){
+		return $this->hasOne('App\Payment_history','id_payment','id')->latest();
+	}
+
+	public function job(){
+		return $this->hasOne('App\Job','id','id_job');
+	}
+
 }
