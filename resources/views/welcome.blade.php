254ec65ddda87a73f077ba2a12b913fafb67975b
rama11 <prof.agastyo@gmail.com>
2020-06-09 09:33:45 +0700

    change Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dispatcher-API
                 </div>
 
                 <div class="links">
