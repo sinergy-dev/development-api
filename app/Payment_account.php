4bb092f9d40804417f7be0127c6ddd3039aee268
rama11 <prof.agastyo@gmail.com>
2020-06-15 11:53:40 +0700

    add model to handel payment account

---
+++ app/Payment_account.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment_account extends Model
+{
+	//
+	protected $connection= 'mysql_dispatcher';
+
+	protected $table = 'payment_account';
+	
+	protected $primaryKey = 'id';
+	
+	public $timestamps = false;
+	
+	protected $fillable = [
+		'id_user',
+		'account_name',
+		'account_number'
+	];
+
+	public function user(){
+		return $this->hasOne('App\User','id','id_user');
+	}
+}
