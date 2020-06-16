750b84a2855bea82295236a635b553382017dd18
rama11 <prof.agastyo@gmail.com>
2020-06-16 16:21:11 +0700

    change testing to API\API controller

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('sendNotification','RestController@sendNotification');
+Route::get('sendNotification','API\APIRestController@sendNotification');
 
 Route::get('/', function () {
     return view('welcome');
