73199648c79808761f732a7de79ebf9f6fdace20
rama11 <prof.agastyo@gmail.com>
2020-06-22 16:29:44 +0700

    add request item on job

---
+++ app/Job_request_item.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Storage;
+
+class Job_request_item extends Model
+{
+    protected $connection= 'mysql_dispatcher';
+
+	protected $table = 'job_request_item';
+	
+	protected $primaryKey = 'id';
+	
+	public $timestamps = false;
+
+	// protected $appends = [
+	// 	'working_engineer',
+	// 	'single_apply_engineer',
+	// ];
+	
+	protected $fillable = [
+		'id_job',
+		'id_engineer',
+		'name_item',
+		'function_item',
+		'documentation_item',
+		'invoice_item',
+		'status_item',
+		'price_item',
+		'date_add'
+	];
+
+}
