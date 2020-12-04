d1cf89f4732adb997bace5ce5f9c74d27426a9fe
ladinar <ladinarnanda@gmail.com>
2020-12-04 09:14:57 +0700

    add date humanize and latest progress

--- app/Payment.php
+++ app/Payment.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Job_category;
+use DB;
+use Carbon\Carbon;
 
 class Payment extends Model
 {
@@ -18,6 +20,8 @@ class Payment extends Model
 	protected $appends = [
 		'payment_invoice_URL',
 		'job_category_image',
+		'date_huminize',
+		'lastest_progress'
 	];
 	
 	protected $fillable = [
@@ -40,8 +44,12 @@ class Payment extends Model
 		return $this->hasMany('App\Payment_history','id_payment','id');
 	}
 
-	public function lastest_progress(){
-		return $this->hasOne('App\Payment_history','id_payment','id')->latest();
+	// public function lastest_progress(){
+	public function getLastestProgressAttribute(){
+		return DB::connection('mysql_dispatcher')->table('payment_history')->where('id_payment',$this->id)->orderBy('id','DESC')->first();
+		// return $this->hasOne('App\Payment_history','id_payment','id')->orderBy(DB::raw('`payment_historya`.`id`','DESCA'));
+
+		// return $this->hasOne('App\Payment_history','id_payment','id')->latest();
 	}
 
 	public function job(){
@@ -50,6 +58,12 @@ class Payment extends Model
 
 	public function getJobCategoryImageAttribute(){
 		return Job_category::find($this->job->id_category)->category_image_url;
+	}
+
+	public function getDateHuminizeAttribute(){
+		$date1 = Carbon::parse(DB::connection('mysql_dispatcher')->table('payment_history')->where('id_payment',$this->id)->orderBy('id','DESC')->first()->date_time);
+		$date2 = Carbon::now();
+		return str_replace("before","ago",$date1->diffForHumans($date2));
 	}
 
 }
